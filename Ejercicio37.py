9bac96d324a84a4821297ff5165308770a7ecc7c
Santiago Sánchez <80766841+SantiagoJSG@users.noreply.github.com>
2021-05-02 23:24:30 -0500

    Update Ejercicio37.py

--- Ejercicio37.py
+++ Ejercicio37.py
@@ -1,2 +1,21 @@
 # Ejercicio 37
-print("El programa está orientado a identificar si al ingresar 3 números, estos en su sencuencia estan aumentando, decreciendo o estan al azar.")
+def contar_digitos_letras(cadena):
+    digitos = 0
+    letras = 0
+
+    for c in cadena:
+        if c.isdigit():
+            digitos += 1
+        elif c.isalpha():
+            letras += 1
+        else:
+            pass
+
+    return digitos, letras
+
+
+texto = input("Digite un texto: ")
+resultado = contar_digitos_letras(texto)
+
+print("Cantidad de dígitos: %i" % resultado[0])
+print("Cantidad de letras: %i" % resultado[1])
