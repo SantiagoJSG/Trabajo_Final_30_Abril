6a3c080b9bf3876550245b2f6e2fcbc1b3c3efc9
Santiago Sánchez <80766841+SantiagoJSG@users.noreply.github.com>
2021-05-02 22:58:16 -0500

    Update Ejercicio29.py

--- Ejercicio29.py
+++ Ejercicio29.py
@@ -1,2 +1,5 @@
 # Ejercicio 29
 print("El programa está orientado a convertir un número a decimal.")
+p = int(input("Ingrese un numero: "))
+deci = float(p)
+print(f"El decimal del numero es: {deci} ")
