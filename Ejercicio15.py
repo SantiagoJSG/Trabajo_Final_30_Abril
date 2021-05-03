d75fb98fde7578956ef99ce44563974828d06010
Santiago Sánchez <80766841+SantiagoJSG@users.noreply.github.com>
2021-05-02 22:57:50 -0500

    Update Ejercicio15.py

--- Ejercicio15.py
+++ Ejercicio15.py
@@ -1,2 +1,5 @@
 # Ejercicio 15
 print("El programa está orientado a calcular la energía en Julios de un objeto.")
+p = float(input("Digite la masa en kilogramos del objeto: "))
+c = 3 * (10 ** 8)
+print("La energia en Julios que hay en un objeto de", p, "kilogramos, es de: ", p * (c ** 2))
